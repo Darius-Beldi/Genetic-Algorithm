6953e5a65fcd6d3d8c2b0c3039c8146b58fb3e27
Darius <beldi_Darius@yahoo.com>
2025-03-31 19:42:28 +0300

    Created a simple gui for easier input

---
+++ gui.py
@@ -0,0 +1,121 @@
+import tkinter as tk
+import main
+
+root = tk.Tk()
+
+# setting the windows size
+root.geometry("1024x1024")
+
+population_size_var = tk.IntVar()
+domain_start_var = tk.IntVar()
+domain_end_var = tk.IntVar()
+a_var = tk.IntVar()
+b_var = tk.IntVar()
+c_var = tk.IntVar()
+precision_var = tk.IntVar()
+crossover_probability_var = tk.IntVar()
+mutation_probability_var = tk.IntVar()
+generation_number_var = tk.IntVar()
+
+
+
+# defining a function that will
+# get the name and password and 
+# print them on the screen
+def submit():
+    population_size = population_size_var
+    domain_start = domain_start_var
+    domain_end = domain_end_var
+    a = a_var
+    b = b_var
+    c = c_var
+    precision = precision_var
+    crossover_probability = crossover_probability_var
+    mutation_probability = mutation_probability_var
+    generation_number = generation_number_var
+
+    max_value = tk.Label(root, text=main.start(population_size_var, domain_start, domain_end, a, b, c, precision,
+                                               crossover_probability, mutation_probability, generation_number)
+                         , font=('calibre', 10, 'bold'))
+
+    max_value.grid(row=29, column=0)
+    text_widget = tk.Text(root, height=70, width=200)
+    with open('Evolutie.txt', 'r') as file:
+        data = file.read()
+        text_widget.insert(tk.END, data)
+
+    text_widget.grid(row=30, column=0, columnspan=0)
+
+
+def default():
+    max_value = tk.Label(root, text=main.start(20,-1,2,-1,1,2,6,25,1,50)
+                         ,font=('calibre', 10, 'bold'))
+
+    max_value.grid(row=29, column=0)
+    text_widget = tk.Text(root, height=70, width=200)
+    with open('Evolutie.txt', 'r') as file:
+        data = file.read()
+        text_widget.insert(tk.END, data)
+
+    text_widget.grid(row=30, column=0, columnspan=2)
+
+
+population_size_label = tk.Label(root, text='Population size', font=('calibre', 10, 'bold'))
+domain_start_label = tk.Label(root, text='Domain start', font=('calibre', 10, 'bold'))
+domain_end_label = tk.Label(root, text='Domain end', font=('calibre', 10, 'bold'))
+a_label = tk.Label(root, text='A', font=('calibre', 10, 'bold'))
+b_label = tk.Label(root, text='B', font=('calibre', 10, 'bold'))
+c_label = tk.Label(root, text='C', font=('calibre', 10, 'bold'))
+precision_label = tk.Label(root, text='Precision', font=('calibre', 10, 'bold'))
+crossover_probability_label = tk.Label(root, text='Crossover', font=('calibre', 10, 'bold'))
+mutation_probability_label = tk.Label(root, text='Mutation', font=('calibre', 10, 'bold'))
+generation_number_label = tk.Label(root, text='Generation', font=('calibre', 10, 'bold'))
+
+
+population_size_entry = tk.Entry(root, textvariable=population_size_var)
+domain_start_entry = tk.Entry(root, textvariable=domain_start_var)
+domain_end_entry = tk.Entry(root, textvariable=domain_end_var)
+a_entry = tk.Entry(root, textvariable=a_var)
+b_entry = tk.Entry(root, textvariable=b_var)
+c_entry = tk.Entry(root, textvariable=c_var)
+precision_entry = tk.Entry(root, textvariable=precision_var)
+crossover_probability_entry = tk.Entry(root, textvariable=crossover_probability_var)
+mutation_probability_entry = tk.Entry(root, textvariable=mutation_probability_var)
+generation_number_entry = tk.Entry(root, textvariable=generation_number_var)
+
+# creating a button using the widget 
+# Button that will call the submit function 
+sub_btn = tk.Button(root, text='Submit', command=submit)
+default_btn = tk.Button(root, text='Default', command=default)
+
+# placing the label and entry in
+# the required position using grid
+# method
+population_size_label.grid(row =0 , column=0, sticky=tk.W)
+population_size_entry.grid(row=0, column=1, sticky=tk.W)
+domain_start_label.grid(row=1, column=0, sticky=tk.W)
+domain_start_entry.grid(row=1, column=1, sticky=tk.W)
+domain_end_label.grid(row=2, column=0, sticky=tk.W)
+domain_end_entry.grid(row=2, column=1, sticky=tk.W)
+a_label.grid(row=3, column=0, sticky=tk.W)
+a_entry.grid(row=3, column=1, sticky=tk.W)
+b_label.grid(row=4, column=0, sticky=tk.W)
+b_entry.grid(row=4, column=1, sticky=tk.W)
+c_label.grid(row=5, column=0, sticky=tk.W)
+c_entry.grid(row=5, column=1, sticky=tk.W)
+precision_label.grid(row=6, column=0, sticky=tk.W)
+precision_entry.grid(row=6, column=1, sticky=tk.W)
+crossover_probability_label.grid(row=7, column=0, sticky=tk.W)
+crossover_probability_entry.grid(row=7, column=1, sticky=tk.W)
+mutation_probability_label.grid(row=8, column=0, sticky=tk.W)
+mutation_probability_entry.grid(row=8, column=1, sticky=tk.W)
+generation_number_label.grid(row=9, column=0, sticky=tk.W)
+generation_number_entry.grid(row=9, column=1, sticky=tk.W, rowspan=2)
+
+
+sub_btn.grid(row=20, column=4, sticky=tk.W, rowspan=2)
+default_btn.grid(row=20, column=5, sticky=tk.W, rowspan=2)
+
+# performing an infinite loop 
+# for the window to display
+root.mainloop()
--- main.py
+++ main.py
@@ -1,155 +1,167 @@
 from functions import *
 from chromosome import *
 
-#MAIN
-already_in = input("Do you want to add new values? Y/N")
-if(already_in == "Y"):
-    population_size = int(input("Population size: "))
-    domain_start = int(input("Domain start: "))
-    domain_end = int(input("Domain end: "))
-    a = int(input("a: "))
-    b = int(input("b: "))
-    c = int(input("c: "))
-    precision = int(input("Precision: "))
-    crossover_probability = int(input("Crossover probability: "))
-    mutation_probability = int(input("Mutation probability: "))
-    generation_number = int(input("Generation number: "))
-    output_file = input("Output file: ")
-else:
-    population_size = 20
-    domain_start = -1
-    domain_end = 2
-    a = -1
-    b = 1
-    c = 2
-    precision = 6
-    crossover_probability = 25
-    mutation_probability = 1
-    generation_number = 50
+# #MAIN
+# already_in = input("Do you want to add new values? Y/N")
+# if(already_in == "Y"):
+#     population_size = int(input("Population size: "))
+#     domain_start = int(input("Domain start: "))
+#     domain_end = int(input("Domain end: "))
+#     a = int(input("a: "))
+#     b = int(input("b: "))
+#     c = int(input("c: "))
+#     precision = int(input("Precision: "))
+#     crossover_probability = int(input("Crossover probability: "))
+#     mutation_probability = int(input("Mutation probability: "))
+#     generation_number = int(input("Generation number: "))
+#     output_file = input("Output file: ")
+# else:
+#     population_size = 20
+#     domain_start = -1
+#     domain_end = 2
+#     a = -1
+#     b = 1
+#     c = 2
+#     precision = 6
+#     crossover_probability = 25
+#     mutation_probability = 1
+#     generation_number = 50
+#     output_file = "Evolutie.txt"
+
+def start(population_size,
+    domain_start,
+    domain_end,
+    a,
+    b,
+    c,
+    precision,
+    crossover_probability,
+    mutation_probability,
+    generation_number):
+
+
     output_file = "Evolutie.txt"
+    file_output = open(output_file, "w")
 
+    file(file_output)
 
-file_output = open(output_file, "w")
+    result = 1
 
-file(file_output)
+    elitist_chromosome = Chromosome(0, 0 ,0)
 
-result = 1
+    #generate the initial population
+    population = generate_population(population_size, domain_start, domain_end, a, b, c, precision)
 
-elitist_chromosome = Chromosome(0, 0 ,0)
+    #print the initial population
+    print_population(population)
 
-#generate the initial population
-population = generate_population(population_size, domain_start, domain_end, a, b, c, precision)
+    #start the generation loop
+    for generation in range(generation_number):
+        max_fitness = -1
+        fitness_sum = 0
+        for chromosome in population:
+            fitness_sum += chromosome.fitness
 
-#print the initial population
-print_population(population)
+        max_fitness = max([chromosome.fitness for chromosome in population])
+        index_max_fitness = [chromosome.fitness for chromosome in population].index(max_fitness)
 
-#start the generation loop
-for generation in range(generation_number):
-    max_fitness = -1
-    fitness_sum = 0
-    for chromosome in population:
-        fitness_sum += chromosome.fitness
+        elitist_chromosome.fitness = population[index_max_fitness].fitness
+        elitist_chromosome.real_value =population[index_max_fitness].real_value
+        elitist_chromosome.binary = population[index_max_fitness].binary
 
-    max_fitness = max([chromosome.fitness for chromosome in population])
-    index_max_fitness = [chromosome.fitness for chromosome in population].index(max_fitness)
+        if(fitness_sum == 0):
+            print("Total fitness = 0 Total Disaster", file = file_output)
+            break
 
-    elitist_chromosome.fitness = population[index_max_fitness].fitness
-    elitist_chromosome.real_value =population[index_max_fitness].real_value
-    elitist_chromosome.binary = population[index_max_fitness].binary
+        partial_sum = []
+        partial_sum.append(0)
+        partial_value = 0.0
 
-    if(fitness_sum == 0):
-        print("Total fitness = 0 Total Disaster", file = file_output)
-        break
-
-    partial_sum = []
-    partial_sum.append(0)
-    partial_value = 0.0
-
-    for j in range(1, population_size + 1):
-        partial_value += population[j-1].fitness
-        partial_sum.append(partial_value / fitness_sum)
-
-    partial_sum.append(1)
-
-    if generation == 0: #checks if we are in the first generation
-        print("Selection probabilities for the first generation: ", file = file_output)
         for j in range(1, population_size + 1):
-            probability = partial_sum[j] - partial_sum[j-1]
-            print("Chromosome: " + str(j) + " with probability: " + str(probability) , file = file_output)
-        print("Possible selection intervals: " , file = file_output)
-        for j in range(population_size + 1):
-            print(partial_sum[j] , end=" " , file = file_output)
-            if j % 3 == 0:
-             print( file = file_output)
+            partial_value += population[j-1].fitness
+            partial_sum.append(partial_value / fitness_sum)
 
-    next_population = []
-    for j in range(population_size):
-        rand = randomFloat(0, 1)
-        left = 0
-        right = population_size - 1
-        while left < right:
-            mid = left + (right - left) // 2
-            if rand <= partial_sum[mid]:
-                right = mid
+        partial_sum.append(1)
+
+        if generation == 0: #checks if we are in the first generation
+            print("Selection probabilities for the first generation: ", file = file_output)
+            for j in range(1, population_size + 1):
+                probability = partial_sum[j] - partial_sum[j-1]
+                print("Chromosome: " + str(j) + " with probability: " + str(probability) , file = file_output)
+            print("Possible selection intervals: " , file = file_output)
+            for j in range(population_size + 1):
+                print(partial_sum[j] , end=" " , file = file_output)
+                if j % 3 == 0:
+                 print( file = file_output)
+
+        next_population = []
+        for j in range(population_size):
+            rand = randomFloat(0, 1)
+            left = 0
+            right = population_size - 1
+            while left < right:
+                mid = left + (right - left) // 2
+                if rand <= partial_sum[mid]:
+                    right = mid
+                else:
+                    left = mid + 1
+            if generation == 0:
+                print("u = " + str(rand) + " chromozone: " + str(left), file = file_output)
+            next_population.append(population[left - 1])
+
+        if generation == 0:
+            print("After selection: ", file = file_output)
+            print_population(next_population)
+
+        crossover_population = []
+        for j in range(population_size):
+            rand = randomFloat(0, 100)
+            if generation == 0:
+                print("Chromosome " + str(j) + " u: " + str(rand) + " binary: " + next_population[j].binary, end =" ", file = file_output)
+            if rand <= crossover_probability:
+                if generation == 0:
+                    print("Selected with the probability: " + str(rand), file = file_output)
+                crossover_population.append(next_population[j])
             else:
-                left = mid + 1
-        if generation == 0:
-            print("u = " + str(rand) + " chromozone: " + str(left), file = file_output)
-        next_population.append(population[left - 1])
+                if generation == 0:
+                    print(file = file_output)
 
-    if generation == 0:
-        print("After selection: ", file = file_output)
-        print_population(next_population)
+        if len(crossover_population) % 2 != 0: #ignore the last one if the crossover population isn't even
+            crossover_population = crossover_population[:-1]
 
-    crossover_population = []
-    for j in range(population_size):
-        rand = randomFloat(0, 100)
-        if generation == 0:
-            print("Chromosome " + str(j) + " u: " + str(rand) + " binary: " + next_population[j].binary, end =" ", file = file_output)
-        if rand <= crossover_probability:
+        for j in range(0, len(crossover_population), 2):
+            chromosome1 = crossover_population[j]
+            chromosome2 = crossover_population[j + 1]
+
+            if generation == 0 :
+                print("Chromosomes: ", file = file_output)
+                print("1) " + chromosome1.binary, file = file_output)
+                print("2) " +  chromosome2.binary, file = file_output)
+
+            chromosome1.binary, chromosome2.binary = crossover(chromosome1.binary, chromosome2.binary, generation)
+            chromosome1.real_value = bits_to_float(chromosome1.binary)
+            chromosome2.real_value = bits_to_float(chromosome2.binary)
+            chromosome1.fitness = evaluate_fitness(chromosome1.real_value, a, b, c)
+            chromosome2.fitness = evaluate_fitness(chromosome2.real_value, a, b, c)
+
             if generation == 0:
-                print("Selected with the probability: " + str(rand), file = file_output)
-            crossover_population.append(next_population[j])
-        else:
-            if generation == 0:
-                print(file = file_output)
-
-    if len(crossover_population) % 2 != 0: #ignore the last one if the crossover population isn't even
-        crossover_population = crossover_population[:-1]
-
-    for j in range(0, len(crossover_population), 2):
-        chromosome1 = crossover_population[j]
-        chromosome2 = crossover_population[j + 1]
-
-        if generation == 0 :
-            print("Chromosomes: ", file = file_output)
-            print("1) " + chromosome1.binary, file = file_output)
-            print("2) " +  chromosome2.binary, file = file_output)
-
-        chromosome1.binary, chromosome2.binary = crossover(chromosome1.binary, chromosome2.binary, generation)
-        chromosome1.real_value = bits_to_float(chromosome1.binary)
-        chromosome2.real_value = bits_to_float(chromosome2.binary)
-        chromosome1.fitness = evaluate_fitness(chromosome1.real_value, a, b, c)
-        chromosome2.fitness = evaluate_fitness(chromosome2.real_value, a, b, c)
+                print("After crossover: ", file = file_output)
+                print_chromosome(chromosome1, 1)
+                print_chromosome(chromosome2, 2)
 
         if generation == 0:
             print("After crossover: ", file = file_output)
-            print_chromosome(chromosome1, 1)
-            print_chromosome(chromosome2, 2)
+            print_population(next_population)
+            print( file = file_output)
+            print("Mutation with the chance: " + str(mutation_probability) + " % for each chromosome", file = file_output)
 
-    if generation == 0:
-        print("After crossover: ", file = file_output)
-        print_population(next_population)
-        print( file = file_output)
-        print("Mutation with the chance: " + str(mutation_probability) + " % for each chromosome", file = file_output)
+        nex_population = mutation_for_all_chromosomes(next_population, mutation_probability, a, b, c, generation)
+        if generation == 0 :
+            print_population(next_population)
 
-    nex_population = mutation_for_all_chromosomes(next_population, mutation_probability, a, b, c, generation)
-    if generation == 0 :
-        print_population(next_population)
+        print("Average fitness for generation: " + str(generation)
+              +" is: " + str(round(sum([chromosometemp.fitness for chromosometemp in next_population]) / len(next_population), precision))
+              + " and the max fitness is: " + str(max_fitness), file = file_output)
+        result = max_fitness
 
-    print("Average fitness for generation: " + str(generation)
-          +" is: " + str(round(sum([chromosometemp.fitness for chromosometemp in next_population]) / len(next_population), precision))
-          + " and the max fitness is: " + str(max_fitness), file = file_output)
-    result = max_fitness
-
-print("The max value of the given function is: " + str(round(result,precision)))
+    return "The max value of the given function is: " + str(round(result,precision))
